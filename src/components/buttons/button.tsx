6a89a513ecf89add567b598cc1ea89a40d72a2ec
ruben-velasquez <rualvemo@hotmail.com>
2023-09-24 18:38:13 -0300

    Improved hero visibility and added icons to buttons

--- src/components/buttons/button.tsx
+++ src/components/buttons/button.tsx
@@ -1,7 +1,7 @@
 export default function Button({ href, children } : ButtonProps) {
   return (
     <a href={href} className="bg-slate-200 shadow-md transition-all hover:scale-105 hover:shadow-lg hover:shadow-blue-500 shadow-slate-500 font-bold py-3 px-8 rounded-full text-slate-900">
-        <button>
+        <button className="flex items-center justify-center gap-2">
             { children }
         </button>
     </a>
--- src/components/structure/hero.tsx
+++ src/components/structure/hero.tsx
@@ -3,10 +3,10 @@ import { Gloock } from "next/font/google";
 import Button from "../buttons/button";
 
 const gloock = Gloock({
-    weight: '400',
-    subsets: ['latin'],
-    display: 'swap',
-  });
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function Hero() {
   return (
@@ -19,15 +19,52 @@ export default function Hero() {
           height={300}
         />
       </div>
-      <h1 className={`${gloock.className} max-w-[600px] text-6xl text-center from-gray-300 via-gray-500 to-slate-300 bg-gradient-to-r bg-clip-text text-transparent`}>
-        A personalized experience with <span className="text-blue-400">AI</span>
+      <h1
+        className={`${gloock.className} max-w-[600px] text-6xl text-center text-white`}
+      >
+        A personalized experience with AI
       </h1>
-      <p className="text-gray-500">Let the AI tell you stories, make your homeworks and even more awesome things.</p>
+      <p className="text-gray-400">
+        Let the AI tell you stories, make your homeworks and even more awesome
+        things.
+      </p>
       <div className="flex gap-2">
         <Button href="#">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="icon icon-tabler icon-tabler-message"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            stroke-width="2"
+            stroke="currentColor"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+            <path d="M8 9h8"></path>
+            <path d="M8 13h6"></path>
+            <path d="M18 4a3 3 0 0 1 3 3v8a3 3 0 0 1 -3 3h-5l-5 3v-3h-2a3 3 0 0 1 -3 -3v-8a3 3 0 0 1 3 -3h12z"></path>
+          </svg>
           Try now
         </Button>
         <Button href="https://github.com/ruben-velasquez/UsefulGPT">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="icon icon-tabler icon-tabler-brand-github"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            stroke-width="2"
+            stroke="currentColor"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+            <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"></path>
+          </svg>
           View Github
         </Button>
       </div>
