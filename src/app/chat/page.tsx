a55d3f3cbc3f0e3652189c849047f67e447dce74
ruben-velasquez <rualvemo@hotmail.com>
2023-10-11 18:57:27 -0300

    Removed chat page background pattern

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -10,16 +10,6 @@ const gloock = Gloock({
 export default function Chat() {
   return (
     <div className="p-5 flex items-center justify-center h-full relative flex-grow transition-all">
-      <div className="absolute -z-1 top-0 w-full">
-        <Image
-          alt="background pattern effect"
-          src="/chat-background-pattern.svg"
-          width={800}
-          height={800}
-          className="h-auto w-full select-none"
-        />
-      </div>
-
       <div className="z-10">
         <h1
           className={`${gloock.className} text-6xl text-center text-chatgpt-textBg`}
