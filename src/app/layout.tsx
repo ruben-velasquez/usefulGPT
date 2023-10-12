8fd0de7957353d76b71f9a110825620aa54f5822
ruben-velasquez <rualvemo@hotmail.com>
2023-10-12 18:52:38 -0300

    Added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,30 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  generator: 'Next.js',
-  title: 'UsefulGPT',
-  colorScheme: 'dark',
-  description: 'This is a sophisticated chatbot interface empowered by OpenAI, designed to provide you with a range of helpful features.',
-  creator: 'Rubén Velásquez',
-}
+  generator: "Next.js",
+  title: "UsefulGPT",
+  colorScheme: "dark",
+  description:
+    "This is a sophisticated chatbot interface empowered by OpenAI, designed to provide you with a range of helpful features.",
+  creator: "Rubén Velásquez",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
-  )
+  );
 }
