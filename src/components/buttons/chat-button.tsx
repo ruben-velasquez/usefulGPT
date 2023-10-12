dcc71196e1252ff2ef0e960753fef6c6707a8a40
ruben-velasquez <rualvemo@hotmail.com>
2023-10-12 14:40:06 -0300

    Chat button accessibility issue fixed

--- src/components/buttons/chat-button.tsx
+++ src/components/buttons/chat-button.tsx
@@ -1,26 +1,21 @@
 "use client";
 import { MouseEventHandler } from "react";
 
-export default function ChatButton({ href, children, onClick, active=true } : ButtonProps) {
-
-  const onClickHandler: MouseEventHandler<HTMLAnchorElement> = (e) => {
-    if(active && onClick) {
-      onClick(e);
-    }
-  }
-
+export default function ChatButton({
+  children,
+  active = true,
+}: ButtonProps) {
   return (
-    <a href={href || "#"} onClick={onClickHandler || ((e) => {})} className="w-fit rounded-full">
-        <button disabled={!active} className="flex items-center justify-center gap-2 w-fit bg-slate-200 transition-all font-bold py-3 px-8 rounded-full text-slate-900">
-            { children }
-        </button>
-    </a>
-  )
+    <button
+      disabled={!active}
+      className="flex items-center justify-center gap-2 w-fit bg-slate-200 transition-all font-bold py-3 px-8 rounded-full text-slate-900"
+    >
+      {children}
+    </button>
+  );
 }
 
 type ButtonProps = {
-    href?: string;
-    onClick?: MouseEventHandler<HTMLAnchorElement>;
-    children: React.ReactNode;
-    active?: boolean;
+  children: React.ReactNode;
+  active?: boolean;
 };
